223002c48ab0df507a5d76d817d5e1df89aafdf0
jhonatan cudris <weiselcudris@hotmail.com>
2022-05-13 00:49:00 -0500

    estructuras de carpeta mas proyecto con gradle

---
+++ src/main/java/com/springj11gradle/platzimarker/PlatziMarkerApplication.java
@@ -0,0 +1,13 @@
+package com.springj11gradle.platzimarker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlatziMarkerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlatziMarkerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springj11gradle/platzimarker/PlatziMarkerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springj11gradle.platzimarker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlatziMarkerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
